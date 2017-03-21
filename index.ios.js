9754c56f8dd3af2c291b7faa9bd30e7771296ee7
Brandon Jones <bjones@appirio.com>
2017-03-21 17:41:24 -0500

    Various UI Updates.

--- index.ios.js
+++ index.ios.js
@@ -14,6 +14,7 @@ import {
   Navigator,
   TouchableHighlight,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   ListView
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -91,6 +92,55 @@ export class HelloWorld extends Component {
   }
 }
 
+
+export class CalendarEvent extends Component{
+
+  constructor(props){
+    super(props);
+    //console.log(this.props);
+    this.state = {
+      isFav: props.event.isFav,
+      favColor: props.event.isFav ? '#F1B931' : "#ccc"
+    };
+    this.toggleFav = this.toggleFav.bind(this);
+    this.onPress = this.onPress.bind(this);
+  }
+
+  toggleFav(){
+    this.setState(prevState => ({
+      isFav: !prevState.isFav,
+      favColor: !prevState.isFav ? '#F1B931' : "#ccc"
+    }));
+  }
+
+  onPress(){
+    //console.log("LETS NAVIGATE!", this.props);
+    this.props.navigator.parentNavigator.push({
+      id: 'HelloWorld',
+      name: 'Hello World'
+    });
+  }
+
+  render() {
+    return (
+      <TouchableWithoutFeedback onPress={this.onPress}>
+      <View style={{flexDirection:'row', borderColor:'rgb(234,234,235)', borderWidth:1}}>
+        <View style={{minWidth:5, backgroundColor:this.props.event.color}}>
+        </View>
+        <View style={{flex:1, flexDirection:'row', justifyContent: 'center', padding:9}}>
+          <View style={{flex:1, justifyContent: 'center'}}>
+            <Text style={{fontWeight:'500'}}>{this.props.event.title}</Text>
+          </View>
+          <View style={{flex:1, justifyContent: 'center', alignItems: 'flex-end'}}>
+          <TouchableOpacity ><Icon name="star" size={20} color={this.state.favColor} /></TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      </TouchableWithoutFeedback>
+    )
+  }
+}
+
 export class USFTest extends Component{
 
   constructor(props) {
@@ -117,7 +167,7 @@ export class USFTest extends Component{
         renderScene={this.renderScene.bind(this)}
         navigator={this.props.navigator}
         navigationBar={
-          <Navigator.NavigationBar style={{backgroundColor : 'white', borderBottomColor: '#ccc', borderBottomWidth: 2, height: 70}}
+          <Navigator.NavigationBar style={{backgroundColor : 'white', borderBottomColor: '#ccc', borderBottomWidth: 1, height: 70}}
             routeMapper={NavigationBarRouteMapper} />
         } />
     );
@@ -152,39 +202,40 @@ export class USFTest extends Component{
     return "#ccc";
   }
 
-  renderEvent(rowData) {
+  toggleFav(rowData){
     for(const event of this.state.dayEvent.event) {
       if(event.time === rowData) {
-        return <View style={{flexDirection:'row', borderColor:'rgb(234,234,235)', borderWidth:1}}>
-          <View style={{minWidth:5, backgroundColor:event.color}}>
-          </View>
-          <View style={{flex:1, flexDirection:'row', justifyContent: 'center', padding:9}}>
-            <View style={{flex:1, justifyContent: 'center'}}>
-              <Text style={{fontWeight:'500'}}>{event.title}</Text>
-            </View>
-            <View style={{flex:1, justifyContent: 'center', alignItems: 'flex-end'}}>
-            <Icon name="star" size={20} color={this.setfav(rowData)} />
-            </View>
-          </View>
-        </View>;
+          event.isFav = !event.isFav;
+          break;
+      }
+    }
+  }
+
+
+  renderEvent(rowData, navigator) {
+    //console.log('RenderEvent Navigator:',navigator);
+    for(const event of this.state.dayEvent.event) {
+      if(event.time === rowData) {
+        return <CalendarEvent event={event} navigator={navigator}/>;
       }
     }
     return <View style={{flex:1}}/>;
   }
 
-  renderRow(rowData) {
+  renderRow(rowData, navigator) {
+    //console.log(navigator);
     return <TouchableHighlight>
         <View>
           <View style={styles.row}>
             <View style={{flex:1}}>
               <View style={{flexDirection:'row'}}>
-                <View style={{paddingLeft:10, paddingRight:15,justifyContent: 'center'}}>
-                <Text style={{fontWeight:"600",justifyContent: 'center',color:this.timeColor(rowData), minWidth:40,}}>
+                <View style={{paddingLeft:10, paddingRight:15, paddingTop: this.timeColor(rowData) == "#ccc" ? 10 : 0, justifyContent: 'center'}}>
+                <Text style={{fontWeight:"600",justifyContent: 'center',color:this.timeColor(rowData), minWidth:40, fontFamily: 'HelveticaNeue-Light'}}>
                   {rowData}
                 </Text>
                 </View>
                 <View style={{flex:1}}>
-                  {this.renderEvent(rowData)}
+                  {this.renderEvent(rowData, navigator)}
                 </View>
               </View>
             </View>
@@ -194,7 +245,8 @@ export class USFTest extends Component{
   }
 
   renderScene(route, navigator){
-    console.log(this.state);
+    //console.log(this.state);
+    //console.log('Navigator: ', navigator);
     return(
         <View style={{flex:1}}>
 
@@ -204,46 +256,54 @@ export class USFTest extends Component{
           <View style={{flexDirection:'row',marginTop:10}}>
 
               <View style={{flex:1, flexDirection:'row', paddingHorizontal:10}}>
-                <View style={{flex:1, justifyContent: 'center'}}><Icon name="angle-left" size={30}  /></View>
-                <View style={{flex:1, justifyContent: 'center'}}><Text style={{fontWeight:"600",justifyContent: 'center' }}>
-                  March
+                <View style={{flex:1, justifyContent: 'center'}}><TouchableOpacity><Icon name="angle-left" size={30} color="#acacac" /></TouchableOpacity></View>
+                <View style={{flex:1, justifyContent: 'center'}}><Text style={{fontWeight:"600",justifyContent: 'center', fontFamily: 'HelveticaNeue-CondensedBold', color: '#333'}}>
+                  MARCH
                 </Text></View>
                 <View style={{flex:1, justifyContent: 'center', alignItems:'flex-end'}}>
-                <Icon name="angle-right" size={30}  />
+                  <TouchableOpacity>
+                    <Icon name="angle-right" size={30} color="#acacac" />
+                  </TouchableOpacity>
                   </View>
-              </View>
+                </View>
               <View style={{flex:1, flexDirection:'row', paddingHorizontal:10}}>
-                <View style={{ justifyContent: 'center'}}><Icon name="angle-left" size={30}  /></View>
+                <View style={{ justifyContent: 'center'}}><TouchableOpacity><Icon name="angle-left" size={30} color="#acacac" /></TouchableOpacity></View>
 
                 <View style={{flex:1, justifyContent: 'center',alignItems:'center'}}>
-                <Text style={{fontWeight:"600",justifyContent: 'center',alignItems:'center' }}>
+                <Text style={{fontWeight:"200",justifyContent: 'center',alignItems:'center', fontFamily: 'HelveticaNeue-Thin' }}>
                     {""+new Date().toLocaleDateString()}
                   </Text>
 
                   </View>
                 <View style={{ justifyContent: 'center', alignItems:'flex-end'}}>
-                  <Icon name="angle-right" size={30}  />
+                  <TouchableOpacity>
+                    <Icon name="angle-right" size={30} color="#acacac" />
+                  </TouchableOpacity>
                 </View>
                 </View>
 
           </View>
         <View style={{marginTop:10,backgroundColor:'#ccc',height:1}}/>
         <ListView dataSource={this.state.timeSlots}
-          renderRow={(rowData) => this.renderRow(rowData)}
+          renderRow={(rowData) => this.renderRow(rowData, navigator)}
           renderSeparator={this._renderSeparator}
+          style={{marginBottom:100}}
         />
 
-        <View style={{marginBottom:20,backgroundColor:'#ccc',minHeight:80,alignItems:'center', flex:1,justifyContent: 'center',flexDirection:'row'}}>
+        <View style={{backgroundColor:'#f4f4f4',minHeight:50,alignItems:'center', flex:1,justifyContent: 'center',flexDirection:'row', alignSelf: 'stretch', position: 'absolute', bottom: 50}}>
 
 
-          <View style={{flex:1, justifyContent: 'center',alignItems:'center'}}>
-              <Text style={{fontWeight:"600",justifyContent: 'center' }}>Days</Text>
+          <View style={{flex:1, justifyContent: 'center',alignItems:'center', height: 50}}>
+              <Text style={{fontWeight:"600",justifyContent: 'center', paddingLeft: 10, alignSelf: 'stretch', textAlign: 'left',fontFamily: 'HelveticaNeue-Light'}}>DAY</Text>
+              <View style={{flex:1, justifyContent: 'center', width: 28, height: 10, backgroundColor: '#5c8727', position: 'absolute', left: 10, bottom: 0}}/>
             </View>
-          <View style={{flex:1, justifyContent: 'center',alignItems:'center'}}>
-              <Text style={{fontWeight:"600",justifyContent: 'center' }}>Week</Text>
+          <View style={{flex:1, justifyContent: 'center',alignItems:'center', height: 50}}>
+              <Text style={{fontWeight:"600",justifyContent: 'center', fontFamily: 'HelveticaNeue-Light', color:'#999' }}>WEEK</Text>
+              <View style={{flex:1, justifyContent: 'center', width: 38, height: 0, backgroundColor: '#5c8727', position: 'absolute',  bottom: 0}}/>
             </View>
-          <View style={{flex:1, justifyContent: 'center',alignItems:'center'}}>
-              <Text style={{fontWeight:"600",justifyContent: 'center' }}>Month</Text>
+          <View style={{flex:1, justifyContent: 'center',alignItems:'center', height: 50}}>
+              <Text style={{fontWeight:"600",justifyContent: 'center', paddingRight: 10,  alignSelf: 'stretch', textAlign: 'right', fontFamily: 'HelveticaNeue-Light', color: '#999'}}>MONTH</Text>
+              <View style={{flex:1, justifyContent: 'center', width: 52, height: 0, backgroundColor: '#5c8727', position: 'absolute', right: 10, bottom: 0}}/>
             </View>
           </View>
 
@@ -265,7 +325,7 @@ var NavigationBarRouteMapper = {
   LeftButton(route, navigator, index, navState){
     return (
       <TouchableOpacity style={{flex: 1, justifyContent: 'center'}}
-        onPress={() => navigator.parentNavigator.pop()}>
+        onPress={() => navigator.parentNavigator.push({id: 'HelloWorld', name: 'Index'})}>
           <Image
             source={require('./img/menubtn.png')}
             style={{ width: 22.5, height: 15, margin: 5}}/>
